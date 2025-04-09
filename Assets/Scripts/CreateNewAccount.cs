fe5ad793a21128eca896f52ce2fee947f6781c54
InnovatorF91 <club_395488567@yahoo.co.jp>
2025-04-10 02:23:15 +0900

    first time commit
    first time commit

---
+++ Assets/Scripts/CreateNewAccount.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e34f366571184eb5d006cf3f95936740286ea24dbcd1d112e4c5f5d58fe8eb86
+size 2155
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:79430e59f47552114684bf78e34fb24d39fde34ee0ea7a962ded0b28e9c6bd4d
+size 4314
